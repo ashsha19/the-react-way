afc19656308cf394a76f0e2acf6b9d5dcf5e5906
ashsha19 <aashishsharma19@gmail.com>
2023-12-25 20:32:42 +0530

    .

--- src/components/storage/read-storage.tsx
+++ src/components/storage/read-storage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IInternalComponentProps } from '../rw-component';
 import { RWDataContext } from '../rw-data-context';
 import { OperationStatus } from '../../context/execution-context';
+import * as hooks from '../../utility/hooks-utility';
 
 interface IReadStorage extends IInternalComponentProps {
     type: 'local' | 'session';
@@ -9,8 +10,14 @@ interface IReadStorage extends IInternalComponentProps {
 }
 
 export function ReadStorage(props: IReadStorage) {
-    const storageType = props.type + 'Storage';
-    const data = window[storageType][props.storageKey];
+    const data = hooks.um(() => {
+        const storageType = props.type + 'Storage';
+        const _data = window[storageType][props.storageKey];
+
+        if (_data !== undefined) {
+            return JSON.parse(_data);
+        }
+    }, [props.storageKey, props.updateSignal]);
 
     return <RWDataContext contextName={props.contextName} data={data} status={OperationStatus.Completed}>{props.children}</RWDataContext>;
 }
--- src/components/storage/write-storage.tsx
+++ src/components/storage/write-storage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IInternalComponentProps } from '../rw-component';
 import { RWDataContext } from '../rw-data-context';
 import { OperationStatus, useRWContext } from '../../context/execution-context';
+import * as hooks from '../../utility/hooks-utility';
 
 interface IWriteStorage extends IInternalComponentProps {
     type: 'local' | 'session';
@@ -17,8 +18,10 @@ export function WriteStorage(props: IWriteStorage) {
         data = lastContext.data;
     }
 
-    const storageType = props.type + 'Storage';
-    window[storageType][props.storageKey] = data;
+    hooks.um(() => {
+        const storageType = props.type + 'Storage';
+        window[storageType][props.storageKey] = JSON.stringify(data);
+    }, [props.storageKey, props.updateSignal, lastContext.updateSignal]);
 
     return <RWDataContext contextName={props.contextName} data={data} status={OperationStatus.Completed}>{props.children}</RWDataContext>;
 }
